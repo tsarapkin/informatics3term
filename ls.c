c5a6c4b70739e09d405b47d9fb452b54ec8332d2
tsarapkin <dangerous1cake@gmail.com>
2015-12-15 20:31:17 +0300

    Ls for you
    works like terminal's ls

---
+++ ls.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <dirent.h>
+#include <errno.h>
+#include <pwd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
+int main(int argc, const char** argv, const char** envp)
+{
+	DIR* dir;
+	struct dirent* curr_file_info;
+	struct stat sb;
+	char* full_path = NULL;
+	struct passwd  *pw_d;
+
+	if(argc < 2){
+		printf("No arguments supplied\n");
+		return -1;
+	}
+
+	dir = opendir(argv[1]);
+
+	if(dir == NULL){
+		printf("Error - %d\n", errno);
+		return -2;
+	}
+
+	full_path = realpath(argv[1], NULL);
+
+	while( ( curr_file_info = readdir(dir) ) != NULL )
+	{
+		printf("Path||Size||Owner||Mode(octal)||File create time\n");
+		printf("%s/%s||", full_path, curr_file_info->d_name);
+		stat((curr_file_info->d_name), &sb)
+    	printf("%lld bytes||", (long long) sb.st_size);
+        pw_d = getpwuid ( sb.st_uid );
+  		printf("%s||", pw_d->pw_name);
+    	printf("%lo||", (unsigned long) sb.st_mode);
+        printf("%s\n", ctime(&sb.st_ctime));
+	}
+
+	free(full_path);
+	closedir(dir);
+	return 0;
+}
